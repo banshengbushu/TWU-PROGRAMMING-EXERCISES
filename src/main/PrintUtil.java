c3653fd3c31ed8e1a43f2c1c3cc07ec66bc36541
Danna Wang <wang.danna@rea-group.com>
2017-09-03 07:54:20 +0800

    Implement printLineAstersisk

--- src/main/PrintUtil.java
+++ src/main/PrintUtil.java
@@ -2,12 +2,19 @@ package main;
 
 public class PrintUtil {
 
-    private void printOneAsterisk(){
+    private void printOneAsterisk() {
         System.out.println("*");
     }
 
-    public static void main(String args[]){
+    private void printLineAsterisk(int n) {
+        for (int i = 0; i <= n; i++) {
+                System.out.printf("*");
+        }
+    }
+
+    public static void main(String args[]) {
         PrintUtil printUtil = new PrintUtil();
         printUtil.printOneAsterisk();
+        printUtil.printLineAsterisk(8);
     }
 }
