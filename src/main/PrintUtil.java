1d3fae0eb39e83b0b209c1fc9ecb243dc0acaf30
Danna Wang <wang.danna@rea-group.com>
2017-09-03 07:34:22 +0800

    Add printUtil and implement printOneAstersisk

---
+++ src/main/PrintUtil.java
@@ -0,0 +1,13 @@
+package main;
+
+public class PrintUtil {
+
+    private void printOneAsterisk(){
+        System.out.println("*");
+    }
+
+    public static void main(String args[]){
+        PrintUtil printUtil = new PrintUtil();
+        printUtil.printOneAsterisk();
+    }
+}
